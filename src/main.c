033df18245b1197d62d201c00ed77d15ba356cf0
Radomir Dopieralski <openstack@sheep.art.pl>
2026-03-22 17:22:56 +0100

    Use all four channels of the first timer, init I2C

--- src/main.c
+++ src/main.c
@@ -1,27 +1,35 @@
-#include "ch32v003fun.h"
+#include "ch32fun.h"
+#include "../ch32fun/examples/i2c_slave/i2c_slave.h"
 #include <stdio.h>
 
 // Number of ticks elapsed per millisecond (48,000 when using 48MHz Clock)
 #define SYSTICK_ONE_MILLISECOND ((uint32_t)FUNCONF_SYSTEM_CORE_CLOCK / 1000)
+// I2C address to listen on
+#define I2C_ADDRESS (0x09)
 
 volatile uint32_t systick_ticks;
+volatile uint8_t i2c_registers[16] = {0x00};
 
 /*
  * initialize TIM1 for PWM
  */
 void t1pwm_init(void) {
-    // Enable GPIOC, GPIOD and TIM1
-    RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC |
-	RCC_APB2Periph_TIM1;
+    // Enable GPIOA, GPIOC, GPIOD and TIM1
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD |
+        RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1;
     // PD2
     GPIOD->CFGLR &= ~(0xf << (4 * 2));
-    GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 2);
-    // PC7
-    //GPIOC->CFGLR &= ~(0xf << (4 * 7));
-    //GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 7);
-    // PC4 is T1CH4, 10MHz Output alt func, push-pull
-    //GPIOC->CFGLR &= ~(0xf << (4 * 4));
-    //GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 4);
+    GPIOD->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 2);
+    // PA1
+    GPIOA->CFGLR &= ~(0xf << (4 * 1));
+    GPIOA->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 1);
+    // PC3
+    GPIOC->CFGLR &= ~(0xf << (4 * 3));
+    GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 3);
+    // PC4
+    GPIOC->CFGLR &= ~(0xf << (4 * 4));
+    GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP_AF) << (4 * 4);
+
     // Reset TIM1 to init all regs
     RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
     RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
@@ -30,7 +38,7 @@ void t1pwm_init(void) {
     // SMCFGR: default clk input is CK_INT
 
     // Prescaler
-    TIM1->PSC = 0x0002;
+    TIM1->PSC = 0x0001;
     // Auto Reload - sets period
     TIM1->ATRLR = 0xffff;
     // One-pulse mode
@@ -39,21 +47,46 @@ void t1pwm_init(void) {
     TIM1->CTLR1 |= TIM_DIR;
     // Reload immediately
     TIM1->SWEVGR |= TIM_UG;
-    // Enable CH1 output, positive pol
-    TIM1->CCER |= TIM_CC1E; //| TIM_CC1P;
-    // Enable CH4 output, positive pol
-    TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+    // Enable CH1 output, negative pol
+    TIM1->CCER |= TIM_CC1E; // | TIM_CC1P;
+    // Enable CH2 output, negative pol
+    TIM1->CCER |= TIM_CC2E; // | TIM_CC2P;
+    // Enable CH3 output, negative pol
+    TIM1->CCER |= TIM_CC3E; // | TIM_CC3P;
+    // Enable CH4 output, negative pol
+    TIM1->CCER |= TIM_CC4E; // | TIM_CC4P;
     // CH1 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
     TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
     // CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+    TIM1->CHCTLR1 |= TIM_OC2M_2 | TIM_OC2M_1;
+    // CH3 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+    TIM1->CHCTLR2 |= TIM_OC3M_2 | TIM_OC3M_1;
+    // CH4 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
     TIM1->CHCTLR2 |= TIM_OC4M_2 | TIM_OC4M_1;
+
     // Set the Capture Compare Register value to 50% initially
     TIM1->CH1CVR = 128;
+    TIM1->CH2CVR = 128;
+    TIM1->CH3CVR = 128;
     TIM1->CH4CVR = 128;
+
     // Enable TIM1 outputs
     TIM1->BDTR |= TIM_MOE;
     // Enable TIM1
     TIM1->CTLR1 |= TIM_CEN;
+}
+
+
+/*
+ *  initialize i2c slave
+ */
+void i2c_init(void) {
+    RCC->APB2PCENR |= RCC_APB2Periph_AFIO; // turn on remapping
+    funPinMode(PC6, GPIO_CFGLR_OUT_10Mhz_AF_OD); // SDA
+    funPinMode(PD5, GPIO_CFGLR_OUT_10Mhz_AF_OD); // SCL
+    AFIO->PCFR1 |= AFIO_PCFR1_I2C1_HIGH_BIT_REMAP; // set high bit = 1  (I2C1REMAP1)
+    AFIO->PCFR1 |= AFIO_PCFR1_I2C1_REMAP; // set low bit [ignored / don't care]    (I2C1_RM)
+    SetupI2CSlave(I2C_ADDRESS, i2c_registers, sizeof(i2c_registers), NULL, NULL, false);
 }
 
 /*
@@ -95,7 +128,7 @@ void systick_init(void) {
 	SYSTICK_CTLR_STIE |	// Enable Interrupts
 	SYSTICK_CTLR_STCLK;	// Set Clock Source to HCLK/1
     // Enable the SysTick IRQ
-    NVIC_EnableIRQ(SysTicK_IRQn);
+    NVIC_EnableIRQ(SysTick_IRQn);
 }
 
 /*
@@ -109,11 +142,16 @@ void SysTick_Handler(void) {
     // If more than this number of ticks elapse before the trigger is reset,
     // you may miss your next interrupt trigger
     // (Make sure the IQR is lightweight and CMP value is reasonable)
-    SysTick->CMP += SYSTICK_ONE_MILLISECOND * 20;
+    SysTick->CMP += SYSTICK_ONE_MILLISECOND * 20; // 50Hz
     // Clear the trigger state for the next IRQ
     SysTick->SR = 0x00000000;
 
     systick_ticks++;
+//    if (systick_ticks & 1) {
+//        AFIO->PCFR1 = AFIO_PCFR1_TIM1_REMAP_NOREMAP;
+//    } else {
+//        AFIO->PCFR1 = AFIO_PCFR1_TIM1_REMAP_FULLREMAP;
+//    }
     TIM1->CTLR1 |= TIM_CEN;
 }
 
@@ -121,10 +159,13 @@ int main() {
     SystemInit();
     funGpioInitAll();
     t1pwm_init();
-    t1pwm_setpw(0, 0x1010);
-    t1pwm_setpw(3, 64);
+    i2c_init();
 
-    //funPinMode(PC4, GPIO_Speed_10MHz | GPIO_CNF_OUT_PP);
+    t1pwm_setpw(0, 0x8cef);
+    t1pwm_setpw(1, 0x3cef);
+    t1pwm_setpw(2, 0x4cef);
+    t1pwm_setpw(3, 6400);
+
     systick_init();
     while (1) {
 	Delay_Ms(250);
